001a281bf3564702df54528590033af25a219fa9
David Caplan <dcaplan@gmail.com>
2017-06-15 19:44:01 -0400

    add DatasetImport.create

--- R/dataset_import.R
+++ R/dataset_import.R
@@ -16,6 +16,7 @@ DatasetImport.all <- function(...) {
     .request('GET', "v1/dataset_imports", query=list(...))
 }
 
+
 #' DatasetImport.retrieve
 #'
 #' Retrieves the metadata about a specific dataset import on SolveBio.
@@ -37,4 +38,55 @@ DatasetImport.retrieve <- function(id) {
 
     path <- paste("v1/dataset_imports", paste(id), sep="/")
     .request('GET', path=path)
+}
+
+
+#' DatasetImport.create
+#'
+#' Create a new dataset import.
+#'
+#' @param dataset_id The target dataset ID.
+#' @param upload_id An upload ID.
+#' @param manifest (optional) A valid file manifest (alternative to upload_id).
+#' @param commit_mode (optional) The commit mode (default: append).
+#' @param auto_approve (optional) Automatically approve the commit (default: TRUE).
+#' @param ... (optional) Additional dataset import attributes.
+#'
+#' @examples \dontrun{
+#' DatasetImport.create(dataset_id=<ID>, upload_id=<ID>)
+#' }
+#'
+#' @references
+#' \url{https://docs.solvebio.com/}
+#'
+#' @export
+DatasetImport.create <- function(dataset_id, upload_id, manifest, commit_mode, auto_approve, ...) {
+    if (missing(dataset_id)) {
+        stop("A dataset ID is required.")
+    }
+
+    if (missing(upload_id) && missing(manifest)) {
+        stop("Either an upload ID or manifest is required.")
+    }
+
+    if (missing(commit_mode)) {
+        commit_mode = 'append'
+    }
+
+    if (missing(auto_approve)) {
+        auto_approve = TRUE
+    }
+
+    params = list(
+                  dataset_id=dataset_id,
+                  upload_id=upload_id,
+                  manifest=manifest,
+                  commit_mode=commit_mode,
+                  auto_approve=auto_approve
+                  ...
+                  )
+
+    dataset_import <- .request('POST', path='v1/dataset_imports', query=NULL, body=params)
+
+    return(dataset_import)
 }
