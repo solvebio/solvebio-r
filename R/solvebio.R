08ad5caa78ec5d3adf19b60dc878be34be0e892b
David Caplan <dcaplan@solvebio.com>
2018-04-04 14:18:51 -0400

    use SOLVEBIO_ACCESS_TOKEN if it exists

--- R/solvebio.R
+++ R/solvebio.R
@@ -9,6 +9,14 @@
 .solveEnv$token <- Sys.getenv('SOLVEBIO_API_KEY', unset='')
 .solveEnv$token_type <- 'Token'
 
+if (nchar(.solveEnv$token) == 0L) {
+    # No API key, look for access token
+    .solveEnv$token <- Sys.getenv('SOLVEBIO_ACCESS_TOKEN', unset='')
+    if (nchar(.solveEnv$token) > 0L) {
+        .solveEnv$token_type <- 'Bearer'
+    }
+}
+
 
 #' login
 #'
--- R/zzz.R
+++ R/zzz.R
@@ -3,4 +3,12 @@
                                  unset='https://api.solvebio.com')
     .solveEnv$token <- Sys.getenv('SOLVEBIO_API_KEY', unset='')
     .solveEnv$token_type <- 'Token'
+
+    if (nchar(.solveEnv$token) == 0L) {
+        # No API key, look for access token
+        .solveEnv$token <- Sys.getenv('SOLVEBIO_ACCESS_TOKEN', unset='')
+        if (nchar(.solveEnv$token) > 0L) {
+            .solveEnv$token_type <- 'Bearer'
+        }
+    }
 }
