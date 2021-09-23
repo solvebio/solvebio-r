20f18bf12bcbaf2f053ce998d28c554e3436b399
tatjana <tatjana@solvebio.com>
2021-09-23 14:19:32 +0200

    Check for API key also

--- R/solvebio.R
+++ R/solvebio.R
@@ -38,6 +38,8 @@ login <- function(api_key, api_host, env = solvebio:::.solveEnv) {
     if (!missing(api_key)) {
         assign('token', api_key, envir=env)
     }
+    .solveEnv$token <- Sys.getenv('SOLVEBIO_API_KEY', unset='')
+    .solveEnv$token_type <- 'Token'
 
     if(nchar(env$token) == 0L) {
         # No API key, look for access token
@@ -46,7 +48,7 @@ login <- function(api_key, api_host, env = solvebio:::.solveEnv) {
             .solveEnv$token_type <- 'Bearer'
         }
         else {
-            stop("No Access Token found. Please set the 'SOLVEBIO_ACCESS_TOKEN' environment variable. Your access token can be found on the Account page of the SolveBio website: https://my.solvebio.com/settings/tokens")
+            stop("No Access Token or API key found. Learn more: https://docs.solvebio.com/#authenticating-with-r")
         }
     }
     if (!missing(api_host)) {
